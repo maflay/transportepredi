2311b722b9a014b51d1c78900f359824502ea1c6
juanfelipe <maflay26@gmail.com>
2026-06-03 21:23:23 -0500

    cambios

--- components/home/index.js
+++ components/home/index.js
@@ -1,29 +1,31 @@
-const btn_salir_home = document.getElementById("btn_salir_home");
+(() => {
+    const btn_salir_home = document.getElementById("btn_salir_home");
 
-btn_salir_home.addEventListener("click", () => {
- Swal.fire({
-    title: "Seguro de salir?",
-    text: "Se olvidara el usuario y la contraseña",
-    icon: "warning",
-    showCancelButton: true,
-    confirmButtonColor: "#3085d6",
-    cancelButtonColor: "#d33",
-    confirmButtonText: "Si Quiero!",
-    allowOutsideClick: false,
-  }).then((result) => {
-    if (result.isConfirmed) {
-      Swal.fire({
-        title: "Exito!",
-        text: "Tu usuario has sido olvidado.",
-        icon: "success",
-        allowOutsideClick: false,
-      }).then((res) => {
-        if (res.isConfirmed) {
-          document.cookie = "1nf0_us3r_tr4ns=; max-age=0; path=/;";
-          window.location.hash = "/validation/login/";
-          location.reload();
-        }
-      });
-    }
-  });
-});
+    btn_salir_home.addEventListener("click", () => {
+        Swal.fire({
+            title: "Seguro de salir?",
+            text: "Se olvidara el usuario y la contraseña",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Si Quiero!",
+            allowOutsideClick: false,
+        }).then((result) => {
+            if (result.isConfirmed) {
+                Swal.fire({
+                    title: "Exito!",
+                    text: "Tu usuario has sido olvidado.",
+                    icon: "success",
+                    allowOutsideClick: false,
+                }).then((res) => {
+                    if (res.isConfirmed) {
+                        document.cookie = "1nf0_us3r_tr4ns=; max-age=0; path=/;";
+                        window.location.hash = "/validation/login/";
+                        location.reload();
+                    }
+                });
+            }
+        });
+    });
+})();
